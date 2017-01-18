f9099552eed6f79d8629fbc1394b8dca9c2b900d
Robert Novo <robert.j.novo@gmail.com>
2017-01-18 20:47:46 +0100

    Labb 1 done

--- lab1/index.js
+++ lab1/index.js
@@ -1,27 +1,31 @@
 $(document).ready(function() {
-
   var dataBlob = {
-    kitten: ['http://i.giphy.com/3oriO0OEd9QIDdllqo.gif', 'http://i.giphy.com/iNMz8LF8y3g4.gif'],
-    puppy: ['http://i.giphy.com/dp7QYhsdgN2Yo.gif', 'http://i.giphy.com/iNMz8LF8y3g4.gif'],
-    bunny: ['http://i.giphy.com/10xUg8DdgQSs9i.gif'],
+    kitten: [
+      "http://i.giphy.com/3oriO0OEd9QIDdllqo.gif",
+      "http://i.giphy.com/iNMz8LF8y3g4.gif"
+    ],
+    puppy: [
+      "http://i.giphy.com/dp7QYhsdgN2Yo.gif",
+      "http://i.giphy.com/iNMz8LF8y3g4.gif"
+    ],
+    bunny: [ "http://i.giphy.com/10xUg8DdgQSs9i.gif" ]
   };
 
-  $('#movie-search').bind('input propertychange', function(event) {
+  $("#movie-search").bind("input propertychange", function(event) {
     var value = event.target.value;
     var hit = dataBlob[value];
 
     if (hit !== undefined) {
       // clear the list of past results.
-      $('#results').empty();
+      $("#results").empty();
 
       // go through each elements in the array for the datablog key
       for (var i = 0; i < hit.length; i++) {
         var currentImage = hit[i];
-        var listElement = '<li><img src="'+ currentImage +'" /></li>';
-        $('#results').append(listElement);
+        var listElement = '<li><img src="' + currentImage + '" /></li>';
+        // adds the list item to the DOM.
+        $("#results").append(listElement);
       }
     }
-
   });
-
 });
