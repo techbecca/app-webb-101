f09613d19b4f095d2c449ccf8c7ae3d116a7c180
Robert Novo <robert.j.novo@gmail.com>
2018-09-13 12:39:16 +0200

    updated lab2 dependencies

--- lab2/webpack.config.js
+++ lab2/webpack.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-    entry: {
-        main: './src/index.js'
-    },
-    output: {
-        filename: './dist/bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: 'babel'
-            }
-        ]
-    }
-}
+	entry: {
+		main: './src/index.js'
+	},
+	output: {
+		filename: './dist/bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader'
+			}
+
+		]
+	}
+};
