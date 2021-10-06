419685cf2f422f54a18bdbc2f683107ca104980e
alizadeh-m <maryam.alizadeh@omegapoint.se>
2021-10-06 09:29:11 +0200

    Fixes for lab2.

--- lab2/src/index.js
+++ lab2/src/index.js
@@ -1,4 +1,3 @@
 import 'babel-polyfill';
-import Promise from 'promise-polyfill';
 import {search} from './search';
 document.querySelector('#animal-search').addEventListener('keyup', search);
--- lab2/webpack.config.js
+++ lab2/webpack.config.js
@@ -1,18 +1,29 @@
-module.exports = {
-	entry: {
-		main: './src/index.js'
-	},
-	output: {
-		filename: './dist/bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				loader: 'babel-loader'
-			}
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-		]
-	}
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader'
+            }
+
+        ]
+    },
+    mode: 'development'
 };
